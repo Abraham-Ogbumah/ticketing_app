2733fd45d43ea1d2beaaf65c0e6d49c4b7a06d6b
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-11-22 16:44:19 -0500

    added Status display component

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import DeleteBlock from "./DeleteBlock";
 import PriorityDisplay from "./PriorityDisplay";
 import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
-      <PriorityDisplay />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay />
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
       <ProgressDisplay />
+      <StatusDisplay />
     </div>
   );
 };
