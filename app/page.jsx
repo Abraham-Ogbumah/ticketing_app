f5c644da30e66a989645e8ef268618e18264df81
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-11-23 19:44:46 -0500

    minor update

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import TicketCard from './(components)/TicketCard'
+import React from "react";
+import TicketCard from "./(components)/TicketCard";
 
 const Dashboard = () => {
   return (
-    <div>
-      <TicketCard />
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
