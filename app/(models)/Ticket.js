7f2389b5ff736cfb5bae01e515856f716b0393b3
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-11-26 17:54:52 -0500

    minor update

--- app/(models)/Ticket.js
+++ app/(models)/Ticket.js
@@ -3,6 +3,8 @@ import mongoose, { Schema } from "mongoose";
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+
 const ticketSchema = new Schema(
   {
     title: String,
